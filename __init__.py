1ab0ada8584b323639088c015711238d74904948
Taremin <taremin.vr@gmail.com>
2021-02-09 12:46:22 +0900

    release 0.0.4

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ bl_info = {
     'author': 'Taremin',
     'location': 'View 3D > UI > Taremin',
     'description': "",
-    'version': (0, 0, 3),
+    'version': (0, 0, 4),
     'blender': (2, 80, 0),
     'wiki_url': '',
     'tracker_url': '',
